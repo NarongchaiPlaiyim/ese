99d3efc10a07b93f5c68cf4102a7301dff6fef02
pakorn <pakorn@the-c-level.com>
2014-11-21 12:58:47 +0700

    update

---
+++ src/main/java/com/ese/model/view/PalletMeanagementView.java
@@ -0,0 +1,60 @@
+package com.ese.model.view;
+
+import com.ese.model.db.ItemModel;
+import com.ese.model.db.LocationModel;
+import com.ese.model.db.WarehouseModel;
+import com.ese.model.db.WorkingAreaModel;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+public class PalletMeanagementView {
+    private int id;
+    private String palletBarcode;
+    private WarehouseModel warehouseModel;
+    private ItemModel itemModel;
+    private LocationModel locationModel;
+    private int tagPrint;
+    private int qty;
+    private int reservedQty;
+    private String status;
+    private String createBy;
+    private Date createDate;
+    private String updateBy;
+    private Date updateDate;
+    private int isValid;
+    private int version;
+    private BigDecimal capacity;
+    private WorkingAreaModel convetorLine;
+    private String shift;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("id", id)
+                .append("palletBarcode", palletBarcode)
+                .append("warehouseModel", warehouseModel)
+                .append("itemModel", itemModel)
+                .append("locationModel", locationModel)
+                .append("tagPrint", tagPrint)
+                .append("qty", qty)
+                .append("reservedQty", reservedQty)
+                .append("status", status)
+                .append("createBy", createBy)
+                .append("createDate", createDate)
+                .append("updateBy", updateBy)
+                .append("updateDate", updateDate)
+                .append("isValid", isValid)
+                .append("version", version)
+                .append("capacity", capacity)
+                .append("convetorLine", convetorLine)
+                .append("shift", shift)
+                .toString();
+    }
+}
