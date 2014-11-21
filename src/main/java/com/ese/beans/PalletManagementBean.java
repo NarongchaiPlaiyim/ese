e2cd42348cd44f20bdbebfb40638cdf25afac6f0
pakorn <pakorn@the-c-level.com>
2014-11-21 15:22:16 +0700

    update

--- src/main/java/com/ese/beans/PalletManagementBean.java
+++ src/main/java/com/ese/beans/PalletManagementBean.java
@@ -11,29 +11,39 @@ import lombok.Setter;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
 @ManagedBean(name = "palletManegement")
 @ViewScoped
-public class PalletManagementBean extends Bean{
+public class PalletManagementBean extends Bean implements Serializable {
 
+    @Getter @Setter
+    List<WarehouseModel> warehouseModelList;
+    @Getter
+    WarehouseModel warehouseMode;
+    @Getter @Setter
+    List<ConveyorLineModel> conveyorLineModelList;
+    @Getter
+    ConveyorLineModel conveyorLineModel;
+    @Getter @Setter
+    List<PalletManagementView> palletMeanegementViewList;
+    @Getter @Setter
+    PalletManagementView palletMeanegementView;
+    @Getter
+    WorkingAreaModel workingAreaModel;
+    @Getter @Setter
+    List<WorkingAreaModel> workingAreaModelList;
 
-    private List<WarehouseModel> warehouseModelList;
-    private WarehouseModel warehouseMode;
-    private List<ConveyorLineModel> conveyorLineModelList;
-    private ConveyorLineModel conveyorLineModel;
-    private List<PalletManagementView> palletManagementViews;
-    private PalletManagementView palletMeanegementView;
-    private WorkingAreaModel workingAreaModel;
-    private List<WorkingAreaModel> workingAreaModelList;
-    private int statusOnShow;
-    private String textTest;
+    @Getter @Setter
+    int statusOnShow;
+    @Getter @Setter
+    String textTest;
 
     @PostConstruct
-    public void onCreation(){
-        log.debug("onCreation().");
+    public void onCreattion(){
+        log.debug("onCreattion().");
         palletMeanegementView = new PalletManagementView();
         warehouseMode = new WarehouseModel();
         conveyorLineModel = new ConveyorLineModel();
@@ -52,12 +62,12 @@ public class PalletManagementBean extends Bean{
 
     private void onloadPallet(){
         log.debug("onloadPallet(). ");
-        palletManagementViews = palletService.findPalletJoinLocation();
+        palletMeanegementViewList = palletService.findPalletJoinLocation();
     }
 
     public void onfind(){
         log.debug("changeOn : {}", statusOnShow);
-        palletManagementViews = palletService.findByChang(statusOnShow, warehouseMode.getId(), workingAreaModel.getId());
+        palletMeanegementViewList = palletService.findByChang(statusOnShow, warehouseMode.getId(), workingAreaModel.getId());
     }
 
 
