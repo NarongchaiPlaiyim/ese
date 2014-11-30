9c4521f7b5a54b0fdb537db363ae98458ce27a22
Chai <narongchai@the-c-level.com>
2014-11-30 14:17:34 +0700

    q

--- src/main/java/com/ese/beans/main.java
+++ src/main/java/com/ese/beans/main.java
@@ -4,8 +4,7 @@ import com.ese.utils.Utils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
@@ -15,28 +14,30 @@ public class main {
         int s = 50;
         int f = 60;
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println(i);
-            while (ss<=ff){
-                System.out.println(ss);
-                if(ss >= s && ss <= f){
-                    System.out.println(ss);
-                    return;
-                }
-                ss++;
-            }
+        List<Integer> list = new ArrayList();
+        for (int i = 1; i < 10; i++) {
+            list.add(i);
         }
 
+        list.add(3);
+        list.add(6);
+        list.add(9);
 
-        System.out.println("DONE");
+        System.out.println(list.toString());
+        Map<String, String> newMap = new HashMap();
 
+        for (Integer i : list){
+            if (!newMap.containsKey(i+"")){
+                newMap.put(i+"", i+"");
+                //add to map
+            } else {
+                System.out.println(i);
+                //delete
+            }
 
-//        for (int i = 0; i < 100; i++) {
-//            if(i >= s && i <= f){
-//                System.out.println(i);
-//            }
-//        }
+        }
 
+        System.out.println(newMap.toString());
     }
 
 
