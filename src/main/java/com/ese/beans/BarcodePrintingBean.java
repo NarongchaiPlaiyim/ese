a886cd0d1b9877807b1a0116e7a037b62a881a33
Chai <narongchai@the-c-level.com>
2014-12-22 12:03:20 +0700

    printing 35%

--- src/main/java/com/ese/beans/BarcodePrintingBean.java
+++ src/main/java/com/ese/beans/BarcodePrintingBean.java
@@ -1,10 +1,13 @@
 package com.ese.beans;
 
+import com.ese.service.BarcodePrintingService;
+import com.ese.utils.Utils;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 @Getter
@@ -13,9 +16,10 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean(name = "barcodePrintingBean")
 public class BarcodePrintingBean extends Bean {
     private static final long serialVersionUID = 4112578634029879990L;
+    @ManagedProperty("#{barcodePrintingService}") private BarcodePrintingService barcodePrintingService;
 
     private String startBarcode;
-    private String qty;
+    private int qty;
     private String finishBarcode;
 
     @PostConstruct
@@ -27,10 +31,29 @@ public class BarcodePrintingBean extends Bean {
     }
 
     private void init(){
+        barcodePrintingService.getLastSeq();//setting to start barcode
+        setStartBarcode("TW00000009");
 
+        setFinishBarcode("TW00000009");
     }
 
     public void onClickOk(){
+        if(!Utils.isZero(getQty())){
+            //before print we have to check seq. of start barcode for other user may click ok by the same start barcode.
 
+        } else {
+            showDialogWarning("QTY must more than 0.");
+        }
+    }
+
+    public void calculator(){
+        final int start = Utils.parseInt(replaceFormat(getStartBarcode()), 0);
+        final int finish = (getQty() + start) - 1;
+        final String result = finish > 999999999 ? "999999999" : String.format("%08d", finish);
+        setFinishBarcode("TW"+ result);
+    }
+
+    private String replaceFormat(String startBarcode){
+        return startBarcode.replace("TW", "");
     }
 }
--- src/main/java/com/ese/service/BarcodePrintingService.java
+++ src/main/java/com/ese/service/BarcodePrintingService.java
@@ -11,4 +11,8 @@ import javax.annotation.Resource;
 public class BarcodePrintingService extends Service{
     private static final long serialVersionUID = 4112577394029874840L;
     @Resource private BarcodePrintingDAO barcodePrintingDAO;
+
+    public String getLastSeq(){
+        return "";
+    }
 }
