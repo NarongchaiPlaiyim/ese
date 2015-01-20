78d29e8c514dbce5a6be1a475a2505ded2110068
Chai <narongchai@the-c-level.com>
2015-01-20 09:26:52 +0700

    Fixed JAN172015-2

--- src/main/java/com/ese/internal/test/Main.java
+++ src/main/java/com/ese/internal/test/Main.java
@@ -10,7 +10,23 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Utils.getDocumentNo());
+        String input = "T0123dc4asd56";
+
+//        if(input.charAt(0) == '(') {
+//            System.out.println(getDigit(input));
+//        }
+    }
+
+    private static String getOnlyDigit(String s) {
+        StringBuilder result = new StringBuilder("");
+        for(int i = 0; i < s.length(); i++) {
+            try {
+                result.append(Integer.parseInt(String.valueOf(s.charAt(i))));
+            } catch (Exception e) {
+
+            }
+        }
+        return result.toString();
     }
 
 
