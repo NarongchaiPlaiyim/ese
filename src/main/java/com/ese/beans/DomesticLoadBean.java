bdc54564a602f80f4025f57f46fdc0e0817eeb9c
Chai <narongchai@the-c-level.com>
2015-03-14 18:21:15 +0700

    bean & service

--- src/main/java/com/ese/beans/DomesticLoadBean.java
+++ src/main/java/com/ese/beans/DomesticLoadBean.java
@@ -1,9 +1,12 @@
 package com.ese.beans;
 
+import com.ese.service.DomesticLoadService;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 @Getter
@@ -12,4 +15,18 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean(name = "domesticLoadBean")
 public class DomesticLoadBean extends Bean {
     private static final long serialVersionUID = 4112578634263394840L;
+    @ManagedProperty("#{domesticLoadService}") private DomesticLoadService domesticLoadService;
+    @ManagedProperty("#{message['authorize.menu.loading.tab.1']}") private String key;
+
+    @PostConstruct
+    public void onCreation(){
+        log.debug("onCreation()");
+        if(preLoad()) {//&& isAuthorize(key)){
+            init();
+        }
+    }
+
+    private void init(){
+
+    }
 }
