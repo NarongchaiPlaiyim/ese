f2682e942c6422714599f79173c9445f05e4e47c
Chai <narongchai@the-c-level.com>
2015-04-02 20:49:47 +0700

    Trigger

---
+++ src/main/java/com/ese/model/db/TriggerModel.java
@@ -0,0 +1,22 @@
+package com.ese.model.db;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Proxy;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "trigger_model")
+@Proxy(lazy=false)
+public class TriggerModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqid-gen")
+    @SequenceGenerator(name = "seqid-gen", sequenceName = "RTDS_ADSINPUT_SEQ", allocationSize = 1, initialValue = 0)
+    private int id;
+
+    @Column(name="isvalid")
+    private int isValid;
+}
